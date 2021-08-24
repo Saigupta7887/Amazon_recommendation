5e1c0fd873a3b9447857b021391b96e84c66ddea
Saigupta7887 <83281595+Saigupta7887@users.noreply.github.com>
2021-08-24 15:03:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,12 @@
 # In[ ]:
 
 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
 import streamlit as st
 import pickle
 import pandas as pd
@@ -37,8 +43,23 @@ def recommend(index):
 if st.button('Show Recommendation'):
 	recommendation=recommend(index)
 	recommendation=pd.DataFrame(recommendation)
+	title_container = st.container()
+	col1, col2 = st.columns([1, 2])
+
+	with title_container:
+          with col1:
+                  st.text(" ")
+
+          with col2:
+            st.image(recommendation['Image'].values[0])
+            st.markdown('<p style="text-align: -webkit-auto; font-weight:bold;">{}</p>'.format(recommendation['Dress_type'].values[0]),
+	unsafe_allow_html=True)
+            st.markdown('<p style="text-align:-webkit-auto;">{}</p>'.format(recommendation['Brand'].values[0]),
+	unsafe_allow_html=True)
+            st.markdown('<p style="text-align: -webkit-auto;"><span style="font-size: 18px;color: #B12704;">₹{}</span>&nbsp;<span style="color: grey; text-decoration: line-through; color:#565959;">₹{}</span></p>'.format(recommendation['Disc_price'].values[0],recommendation['Actual_price'].values[0]), unsafe_allow_html=True)
 	
-	count = 0
+
+	count = 1
 	for i in range(4):
 	    for j in st.columns(3):
 
